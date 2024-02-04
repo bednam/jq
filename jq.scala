38a0445b29432d7f4e64399586d3e4b73a0afc31
Michal Bednarz <mbednarz@evolution.com>
2024-02-04 10:51:17 +0000

    parse basic filter and input

--- jq.scala
+++ jq.scala
@@ -8,25 +8,24 @@ import com.monovore.decline.*
 import com.monovore.decline.effect.*
 import io.circe.*
 import io.circe.parser.*
-import fs2.io.* 
+import io.circe.syntax.*
+import fs2.io.*
 import fs2.Stream.eval
 
-object Main extends IOApp.Simple {
-    case class ParseJson(filter: String)
+object Main extends CommandIOApp(name = "jq", header = "jq")  {
+  val filterOpts: Opts[String] = Opts.argument[String](metavar = "filter")
+  val inputOpts: Opts[String] = Opts.argument[String](metavar = "input")
 
-    val filterOpts: Opts[String] = Opts.argument[String](metavar = "filter")
-    def run = 
-        for {
-            result <- stdinUtf8[IO](1024).compile.lastOrError
-            json <- IO.fromEither(parse(result))
-            _ <- IO.println(json)
-        } yield ()
-    // def run(args: List[String]): IO[ExitCode] = {
+  def program(filter: String, input: String) = for {
+    _ <- IO.println(filter)
+    json <- IO.fromEither(parse(input))
+    result = json \\ filter
+    _ <- IO.println(result.asJson)
+  } yield ExitCode.Success
 
-    //     val json = args.head
-    //     println(json)
-    //     IO.println(parse(json))
-    // }.as(ExitCode.Success)
-
+  def main: Opts[IO[ExitCode]] =
+    (filterOpts, inputOpts).mapN(program)
 }
+
+// scala-cli jq.scala -- 'quotes' '{"quotes":[{"id":1,"quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},{"id":2,"quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"}],"total":100,"skip":0,"limit":2}'
 
